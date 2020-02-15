c12e64f4911fdc11732c3e2aa5c5aaeef82c0c4e
Mark <ratrateroo@gmail.com>
2020-02-15 20:38:28 +0800

    started using redux

--- redux only/app.js
+++ redux only/app.js
@@ -1,1 +1,21 @@
 const { createStore } = require('redux');
+
+const intialState = {
+    age: 30
+}
+
+const myReducer = (state = intialState, action) => {
+    const newState = { ...state };
+
+    if( action.type === 'ADD') {
+        newState.age += 1;
+    }
+    
+    return newState;
+}
+
+const store = createStore(myReducer);
+
+store.dispatch({ type: 'ADD' });
+
+console.log(store.getState());
