6522bce19607f7bafacc9d63f6281f14a530ef5e
Mark <ratrateroo@gmail.com>
2020-02-15 22:17:20 +0800

    added the age state

--- react redux/react-redux/src/App.js
+++ react redux/react-redux/src/App.js
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [age, setAge] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Age:<span></span></div>
+      <button>Age Up</button>
+      <button>Age Down</button>
     </div>
   );
 }
