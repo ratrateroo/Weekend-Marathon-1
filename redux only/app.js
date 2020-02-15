4d903337677b1e3a1af631fdac5b51421a2af067
Mark <ratrateroo@gmail.com>
2020-02-15 21:13:14 +0800

    added subscription

--- redux only/app.js
+++ redux only/app.js
@@ -19,8 +19,11 @@ const myReducer = (state = intialState, action) => {
 
 const store = createStore(myReducer);
 
+store.subscribe(() => {
+    console.log('The store has changed: ' + JSON.stringify(store.getState()));
+});
+
 console.log(store.getState());
 store.dispatch({ type: 'ADD' });
-console.log(store.getState());
 store.dispatch({ type: 'SUBTRACT' });
 console.log(store.getState());
