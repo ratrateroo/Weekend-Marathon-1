fc343436596d171ad74fefeb764225c923f35859
Mark <ratrateroo@gmail.com>
2020-02-16 03:01:52 +0800

    fixed the app components

--- react redux/react-redux/src/Age.js
+++ react redux/react-redux/src/Age.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
+import CurrentAge from './components/CurrentAge';
+import Buttons from './components/Buttons';
 
 
 const Age = () => {
@@ -24,47 +26,4 @@ const Age = () => {
 
 export default Age;
 
-  const [age, setAge] = useState(30);
-  const ageUpHandler = () => {
-    setAge(age + 1);
-  }
-
-  const ageDownHandler = () => {
-    setAge(age - 1);
-  }
-
-
-
-function App() {
-  const [age, setAge] = useState(30);
-  const ageUpHandler = () => {
-    setAge(age + 1);
-  }
-
-  const ageDownHandler = () => {
-    setAge(age - 1);
-  }
-  return (
-    <div className="App">
-      <div>Age: <span>{age}</span></div>
-      <button onClick={ ageUpHandler }>Age Up</button>
-      <button onClick={ ageDownHandler }>Age Down</button>
-    </div>
-  );
-}
-
-const mapStateToProps = (age) => {
-  return {
-    age: age
-  }
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    onAgeUp: () => dispatch({type: 'AGE_UP'}),
-    onAgeDown: () => dispatch({type: 'AGE_DOWN'})
-  }
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
-
+  
--- react redux/react-redux/src/App.js
+++ react redux/react-redux/src/App.js
@@ -1,36 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Age from './Age';
 import './App.css';
-import { connect } from 'react-redux';
 
-function App() {
-  const [age, setAge] = useState(30);
-  const ageUpHandler = () => {
-    setAge(age + 1);
-  }
+const App = () => {
+    
 
-  const ageDownHandler = () => {
-    setAge(age - 1);
-  }
   return (
-    <div className="App">
-      <div>Age: <span>{age}</span></div>
-      <button onClick={ ageUpHandler }>Age Up</button>
-      <button onClick={ ageDownHandler }>Age Down</button>
+    <div class="App">
+      <Age/>   
     </div>
   );
-}
-
-const mapStateToProps = (age) => {
-  return {
-    age: age
-  }
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    onAgeUp: () => dispatch({type: 'AGE_UP'}),
-    onAgeDown: () => dispatch({type: 'AGE_DOWN'})
-  }
+  
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
+
+  
