5c7302624a3a332c8d8af75286d6f25ac95ede01
Mark <ratrateroo@gmail.com>
2020-02-15 22:32:19 +0800

    updated the functions

--- react redux/react-redux/src/App.js
+++ react redux/react-redux/src/App.js
@@ -3,11 +3,18 @@ import './App.css';
 
 function App() {
   const [age, setAge] = useState(30);
+  const ageUpHandler = () => {
+    setAge(age + 1);
+  }
+
+  const ageDownHandler = () => {
+    setAge(age - 1);
+  }
   return (
     <div className="App">
       <div>Age: <span>{age}</span></div>
-      <button onClick={() => setAge(age + 1)}>Age Up</button>
-      <button onClick={() => setAge(age - 1)}>Age Down</button>
+      <button onClick={ ageUpHandler }>Age Up</button>
+      <button onClick={ ageDownHandler }>Age Down</button>
     </div>
   );
 }
