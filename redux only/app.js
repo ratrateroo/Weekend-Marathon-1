e99750846977faae853a22edeaab02c300b6703d
Mark <ratrateroo@gmail.com>
2020-02-15 20:58:11 +0800

    tried some more dispatch

--- redux only/app.js
+++ redux only/app.js
@@ -10,12 +10,17 @@ const myReducer = (state = intialState, action) => {
     if( action.type === 'ADD') {
         newState.age += 1;
     }
+    if( action.type === 'SUBTRACT') {
+        newState.age -= 1;
+    }
     
     return newState;
 }
 
 const store = createStore(myReducer);
 
+console.log(store.getState());
 store.dispatch({ type: 'ADD' });
-
+console.log(store.getState());
+store.dispatch({ type: 'SUBTRACT' });
 console.log(store.getState());
