3fe05ec9435165393887b056ed861d11968dbb09
Mark <ratrateroo@gmail.com>
2020-02-17 03:44:27 +0800

    added provider and store in the app

--- redux-with-hooks/src/App.js
+++ redux-with-hooks/src/App.js
@@ -2,12 +2,18 @@ import React from 'react';
 import './App.css';
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
+
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
 function App() {
   return (
-    <div className = "main">
-      <TodoInput />
-      <TodoList />
-    </div>
+    <Provider store = { store }>
+      <div className = "main">
+        <TodoInput />
+        <TodoList />
+      </div>
+    </Provider>
   );
 }
 
