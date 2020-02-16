1036cfb939cc5733d22cd5275427eb680cd3f10a
Mark <ratrateroo@gmail.com>
2020-02-17 03:35:01 +0800

    added store actions and reducer

--- redux-with-hooks/src/redux/store.js
+++ redux-with-hooks/src/redux/store.js
@@ -1,2 +1,20 @@
 import { createStore } from 'redux';
 import uuid from 'uuid/v4';
+import reducer from './reducer/reducer';
+
+const initialState = {
+    todos: [
+      {
+        id: uuid(),
+        name: 'Go to the mall',
+        complete: false
+      },
+      {
+        id: uuid(),
+        name: 'Do coding',
+        complete: true
+      }
+    ]
+  };
+
+export default store = createStore(reducer);
