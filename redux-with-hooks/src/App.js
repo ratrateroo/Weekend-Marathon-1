8c4fcdb12b0f17b99fe5aaa912f131b1d287f39c
Mark <ratrateroo@gmail.com>
2020-02-17 03:03:59 +0800

    added two components to the app

--- redux-with-hooks/src/App.js
+++ redux-with-hooks/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
-
+import TodoInput from './components/TodoInput';
+import TodoList from './components/TodoList';
 function App() {
   return (
     <div className = "main">
-      
+      <TodoInput />
+      <TodoList />
     </div>
   );
 }
